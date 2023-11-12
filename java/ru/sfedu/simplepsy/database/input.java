fbbb93a8bf403b48e2f450d98936bbd8f40b172b
Mikhail <my.rs@mail.ru>
2023-11-12 16:01:41 +0300

    удалил папку .idea, добавил корневую директорию и изменил package во всех исходниках

---
+++ java/ru/sfedu/simplepsy/database/input.java
@@ -0,0 +1,31 @@
+package ru.sfedu.simplepsy.database;
+
+import java.sql.*;
+import java.io.*;
+
+public class input {
+    public static void main(String[] args) {
+        String url = "jdbc:your_database_url";
+        String username = "your_username";
+        String password = "your_password";
+
+        Connection connection;
+        Statement statement;
+        String table_name = "table_name";
+        String condition = "condition";
+        String input_query = "INPUT INTO " + table_name + " FROM " + condition;
+        try {
+            connection = DriverManager.getConnection(url, username, password);
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+            throw new RuntimeException();
+        }
+        try {
+            statement = connection.createStatement();
+            statement.executeQuery(input_query);
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+            throw new RuntimeException();
+        }
+    }
+}
