be36323ed27817d81ed3886dda097d1bfafc8402
Mikhail <my.rs@mail.ru>
2023-11-19 12:29:27 +0300

    создание клиента или возвращение id клиента

--- java/ru/sfedu/simplepsy/database/DataProviderMeeting.java
+++ java/ru/sfedu/simplepsy/database/DataProviderMeeting.java
@@ -6,37 +6,39 @@ import java.sql.SQLException;
 
 class DataProviderMeeting{
 
-	public static void createMeetingTable(Connection connect){
-		String query = """
-		DROP TABLE IF EXISTS meetings ;
+	public static void createMeetingTable(String username, String password){
+	
+		try(Connection connect = PsyDB.getConnection(username, password)){
+			String query = """
+			DROP TABLE IF EXISTS meetings ;
 
-		CREATE TABLE meetings (
-			id integer CONSTRAINT primary_key PRIMARY KEY,
-			lastMeetingDateTime timestamp,
-    			nextMeetingDateTime timestamp,
-    			meetingFormat varchar(128),
-    			clientRequest text,
-    			therapistState varchar(256),
-    			topicsDiscussed text,
-    			clientInsights text,
-    			clientEmotions text,
-    			therapistEmotions text,
-    			therapistUnexpressedEmotions text,
-    			techniquesAndMethods text,
-    			obstaclesAndResistance text,
-    			therapistStateAfterSession varchar(256),
-    			planForNextSession text,
-    			difficultiesAndSupervisionTopics text,
-    			postponed boolean,
-    			countertransference boolean
-		)""";
+			CREATE TABLE meetings (
+				id integer CONSTRAINT primary_key PRIMARY KEY,
+				lastMeetingDateTime timestamp,
+	    			nextMeetingDateTime timestamp,
+	    			meetingFormat varchar(128),
+	    			clientRequest text,
+	    			therapistState varchar(256),
+	    			topicsDiscussed text,
+	    			clientInsights text,
+	    			clientEmotions text,
+	    			therapistEmotions text,
+	    			therapistUnexpressedEmotions text,
+	    			techniquesAndMethods text,
+	    			obstaclesAndResistance text,
+	    			therapistStateAfterSession varchar(256),
+	    			planForNextSession text,
+	    			difficultiesAndSupervisionTopics text,
+	    			postponed boolean,
+	    			countertransference boolean
+			)""";
 
-		try{
-			Statement statement = connect.createStatement();
-			statement.executeQuery(query);
-		} catch(SQLException e){
-			e.printStackTrace();
+			try{
+				Statement statement = connect.createStatement();
+				statement.executeQuery(query);
+			} catch(SQLException e){
+				e.printStackTrace();
+			}
 		}
 	}
-
 }
