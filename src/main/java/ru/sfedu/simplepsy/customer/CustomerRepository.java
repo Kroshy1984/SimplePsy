d1114152c2593ba7a0db7f974079d6789b2b5b3f
Breadlyy <kirilllozanov012@gmail.com>
2023-12-18 19:33:08 +0300

    поиск кастомера по имени или контакту или и тому и тому. Добавлен ControllerAdvice и респонс в случае неудачного поиска

--- src/main/java/ru/sfedu/simplepsy/customer/CustomerRepository.java
+++ src/main/java/ru/sfedu/simplepsy/customer/CustomerRepository.java
@@ -10,6 +10,8 @@ public interface CustomerRepository extends MongoRepository<Customer, String> {
         @Query("{'name': '?0', $or: [{'contact.phone': '?1'}, {'contact.email':'?1'}, {'tg': '?1'}] }")
         Optional<Customer> findByNameAndSomeContact(String name, String contact);
 
-
+        @Query("{ $or: [ {'contact.phone': ?0}, {'contact.email': ?0}, {'contact.tg':  ?0}] }")
+        Optional<Customer> findCustomerByContact(String contact);
+        Optional<Customer> findCustomerByName(String name);
 
 }
--- src/main/java/ru/sfedu/simplepsy/customer/CustomerService.java
+++ src/main/java/ru/sfedu/simplepsy/customer/CustomerService.java
@@ -2,7 +2,7 @@ package ru.sfedu.simplepsy.customer;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import ru.sfedu.simplepsy.NotFoundException;
+import ru.sfedu.simplepsy.exception.NotFoundException;
 
 @Service
 public class CustomerService {
@@ -10,12 +10,23 @@ public class CustomerService {
     @Autowired
     private CustomerRepository customerRepository;
 
-    public Customer getCustomer(String name, String contact) {
+    public Customer getCustomerByNameAndContact(String name, String contact) {
         return customerRepository
                 .findByNameAndSomeContact(name, contact)
                 .orElseThrow(() -> new NotFoundException("Customer not found"));
     }
-
+    public Customer getCustomerByName(String name)
+    {
+        return customerRepository
+                .findCustomerByName(name)
+                .orElseThrow(() -> new NotFoundException(String.format("Customer with name %s not found", name)));
+    }
+    public Customer getCustomerByContact(String contact)
+    {
+        return customerRepository
+                .findCustomerByContact(contact)
+                .orElseThrow(() -> new NotFoundException(String.format("Customer with contact %s not found", contact)));
+    }
     public Customer saveCustomer(Customer customer) {
         return customerRepository.insert(customer);
     }
