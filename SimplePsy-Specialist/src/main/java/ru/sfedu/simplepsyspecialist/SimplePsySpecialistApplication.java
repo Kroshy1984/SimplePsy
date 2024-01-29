815616d1fbfe7c7b0cddd884fc4276180ff4ff0e
Breadlyy <kirilllozanov012@gmail.com>
2024-01-29 19:41:26 +0300

    commit gradle

---
+++ SimplePsy-Specialist/src/main/java/ru/sfedu/simplepsyspecialist/SimplePsySpecialistApplication.java
@@ -0,0 +1,13 @@
+package ru.sfedu.simplepsyspecialist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplePsySpecialistApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimplePsySpecialistApplication.class, args);
+    }
+
+}
---
+++ scoring/src/main/java/ru/sfedu/scoring/scoring/ScoringApplication.java
@@ -0,0 +1,13 @@
+package ru.sfedu.scoring.scoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScoringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScoringApplication.class, args);
+	}
+
+}
