382c35eb83d13a97da48398e9aa80e94b45f7949
shmvsky <shmvsky1337@gmail.com>
2024-04-27 22:28:52 +0300

    adds dockerfiles

--- src/main/java/ru/sfedu/simplepsycustomer/simplepsy/exception/CustomerNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package ru.sfedu.simplepsycustomer.simplepsy.exception;
-
-public class CustomerNotFoundException extends RuntimeException{
-    public CustomerNotFoundException(String message)
-    {
-        super(message);
-    }
-}
--- src/main/java/ru/sfedu/simplepsycustomer/simplepsy/specialist/SpecialistRepository.java
+++
@@ -1,7 +0,0 @@
-package ru.sfedu.simplepsycustomer.simplepsy.specialist;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-
-public interface SpecialistRepository extends MongoRepository<Specialist, String> {
-}
