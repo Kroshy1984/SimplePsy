e2599c3fba9eed4e05278b105a1fdb5f785e4e78
Breadlyy <kirilllozanov012@gmail.com>
2024-05-03 14:48:50 +0300

    добавлена кнопка для копирования ссылки на скоринг в списке проблем кастомера

--- SimplePsy-Specialist/src/main/resources/static/js/problems-list.js
+++ SimplePsy-Specialist/src/main/resources/static/js/problems-list.js
@@ -8,3 +8,20 @@ function addNewProblem() {
     window.location.href = "http://localhost:8081/SimplePsySpecialist/V1/specialist/customer/problem/new/" + id;
 }
 
+function copyUrl() {
+
+    var selectedRadioButton = document.querySelector('input[name="selectedItem"]:checked');
+
+
+    if (selectedRadioButton) {
+
+        var selectedProblemId = selectedRadioButton.value;
+
+
+        var viewUrl = 'http://localhost:8084/SimplePsyScoring/V1/scoring/' + selectedProblemId;
+
+    } else {
+        alert('Выберите проблемы перед копированием ссылки.');
+    }
+    navigator.clipboard.writeText(viewUrl).then(() => alert("Ссылка скопирована"));
+}
