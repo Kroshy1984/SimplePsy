1cc6975dd87fe81c6bb12bbadd4693517d8755cf
Heynord <igla2307@gmail.com>
2024-08-30 11:02:03 +0300

    Фикс редактирования тестов/опросов

--- SimplePsy-Specialist/src/main/java/ru/sfedu/simplepsyspecialist/controller/ScoringController.java
+++ SimplePsy-Specialist/src/main/java/ru/sfedu/simplepsyspecialist/controller/ScoringController.java
@@ -189,7 +189,7 @@ public class ScoringController {
     public String updateTest(@PathVariable String id, @RequestBody Scoring scoring) {
         scoring.setId(id);
         scoring.getQuestions().forEach(question -> System.out.println(question.getQuestionText()));
-        scoringService.save(scoring);
+        scoringService.update(scoring);
         return "redirect:/list"; // перенаправление на список тестов или другой нужный маршрут
     }
     private static Gender convertSexToGender(Sex sex) {
